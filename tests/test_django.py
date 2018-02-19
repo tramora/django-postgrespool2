9104e6a60f87a9746278b8eb1e47c52c2dd21be3
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2018-02-19 10:04:20 +0300

    Added test for postgis

--- tests/test_django.py
+++ tests/test_django.py
@@ -10,3 +10,15 @@ class TestPool(TestCase):
 
     def test_simple_request(self):
         pass
+
+    def test_postgis(self):
+        with self.settings(DATABASES={
+            'default': {
+                'ENGINE': 'django_postgrespool2.postgis',
+                'NAME': 'postgrespool_test',
+                'USER': 'postgres',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+            }
+        }):
+            DogModel.objects.all()
