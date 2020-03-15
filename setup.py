4f11ccb82fa35668104f6cf9f94d5f331003c017
Sascha P <chickahoona@users.noreply.github.com>
2020-03-15 10:41:38 +0100

    Fix #15: README.rst encoding issue during pip install (#16)

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from django_postgrespool2 import __version__, __author__
+import codecs
 
 required = [
     'sqlalchemy>=1.1',
@@ -12,7 +13,7 @@ setup(
     name='django-postgrespool2',
     version=__version__,
     description='Postgres Connection Pooling for Django.',
-    long_description=open('README.rst').read(),
+    long_description=codecs.open('README.rst', 'r', 'utf-8').read(),
     author=__author__,
     author_email='malexey1984@gmail.com',
     url='https://github.com/lcd1232/django-postgrespool2',
